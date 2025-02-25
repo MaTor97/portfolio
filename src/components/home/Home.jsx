f6ca6a13eb4ffdead284a32c798b9cd836a23af2
MaTor97 <mttortora97@gmail.com>
2025-02-25 15:15:11 +0100

    add border to skills section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,7 +5,7 @@ const Home = ({language, setpage}) => {
     return (
         <div className="home">
             {language === 'FR' 
-                ? <p>Matteo Tortora est un développeur web qui cherche à se spécialiser en React tout en construisant son arsenal d'outils pour devenir full-stack. À l'écoute des nouvelles opportunités et toujours partant, c'est un avenir de découvertes et d'évolutions qui l'attend. <span onClick={() => setpage('cv')}>ici</span>.</p>                 
+                ? <p>Matteo Tortora est un développeur web qui cherche à se spécialiser en React tout en construisant son arsenal d'outils pour devenir full-stack. À l'écoute des nouvelles opportunités et toujours partant, c'est un avenir de découvertes et d'évolutions qui l'attend. Vous pouvez retrouver son parcours professionnel <span onClick={() => setpage('cv')}>ici</span>.</p>                 
             : language === 'EN' 
                 ? <p>Matteo Tortora is a web developer aiming to specialize in React while expanding his toolkit to become full-stack. Open to new opportunities and always ready, a future of discoveries and growth awaits him. You can find his professional journey <span onClick={() => setpage('cv')}>here</span>.</p> 
             : language === 'IT' 
