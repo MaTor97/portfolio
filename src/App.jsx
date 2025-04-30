53e96c624cd8e78300a2f360948c9f793d70d933
MaTor97 <mttortora97@gmail.com>
2025-04-30 09:58:47 +0200

    web view war debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import WebViewWarning from './components/WebViewWarning'
 import Header from './components/Header'
 import Nav from './components/Nav'
